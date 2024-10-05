d7b6fac02be013743572fe87fd6705cdcaa79abd
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-10-05 22:58:00 +0530

    pagination in react

---
+++ L11_Pagination/tanstack-project/src/App.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./components/Layout/MainLayout";
+import Home from "./components/pages/Home";
+import FetchOld from "./components/pages/FetchOld";
+import FetchRQ from "./components/pages/FetchRQ";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import DetailsPage from "./components/pages/DetailsPage";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/trad",
+                element: <FetchOld />,
+            },
+            {
+                path: "rq/posts",
+                element: <FetchRQ />,
+            },
+            {
+                path: "rq/:id",
+                element: <DetailsPage />,
+            },
+        ],
+    },
+]);
+
+function App() {
+    const queryClient = new QueryClient();
+    return (
+        <div className="container mx-auto">
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
+        </div>
+    );
+}
+
+export default App;
---
+++ L11_Pagination/tanstack-project/src/api/api.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+export const fetchPosts = (page) => api.get(`/posts?_page=${page}&_limit=3`);
+
+export const fetchIndiPost = (id) => api.get(`/posts/${id}`);
